3af5bab1573e1e1dc1f8dc70279ef2c7dc0bfe94
sucoLT <lukas.melcher@gmail.com>
2022-04-30 13:41:45 +0200

    m

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -35,16 +35,61 @@ function pAquorFactory(num, strand) {
 
         }
       }
+    },
+
+    // compare yourself Dna with another .... how many bases in same location are identical? console log how many % are identical
+
+    compareDna(pAenquor) {
+      // increment number with for
+      let numberOfSameBases = 0;
+      for (i = 0; i < pAenquor.dna.length; i++) {
+        if (pAenquor.dna[i] === this.dna[i]) {
+          numberOfSameBases++;
+        }
+      }
+      /*console.log("owndna", this.dna);
+      console.log("otherdna", pAenquor.dna);
+      console.log("sameBases", numberOfSameBases)*/
+      console.log(`specimen Nr. ${this.specimenNum} and specimen Nr. ${pAenquor.specimenNum} have ${numberOfSameBases / 15 * 100}% of DNA in common `)
+    },
+
+    willLikelySurvive() {
+      let sumOfCG = 0;
+      console.log("its dna", this.dna);
+      this.dna.forEach(element => {
+        switch (element) {
+          case "C":
+            sumOfCG++;
+            break;
+          case "G":
+            sumOfCG++;
+            break;
+        }
+      });
+
+      console.log(sumOfCG);
+      console.log(sumOfCG / 15 * 100);
+      if (sumOfCG / 15 * 100 >= 60) {
+        return true;
+      } else {
+        return false;
+      }
     }
 
   }
 }
 
+
+
 // testing
 //console.log("randBase", returnRandBase());
 //console.log("strand", mockUpStrand());
 creature = pAquorFactory(1, mockUpStrand());
-console.log("mutate", creature.mutate());
+creature2 = pAquorFactory(2, mockUpStrand());
+console.log(creature.willLikelySurvive());
+
+//creature.compareDna(creature2);
+//console.log("mutate", creature.mutate());
 
 
 
